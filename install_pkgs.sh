66b12aa55b3e5d53a543ea7380c8cd4c83f48f67
Daniele Coppola <daniele.coppola@inf.ethz.ch>
2025-01-05 11:14:27 +0100

    pkg for screen shots

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -5,5 +5,5 @@ sudo apt group install sway-desktop-environment
 sudo apt install mako
 sudo apt install alacritty
 sudo apt install wofi
-sudo apt install waybar 
+sudo apt install waybar  grimshot
 sudo apt install tmux
