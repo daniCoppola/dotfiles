650ed0da3eab8bb74f8a46c5a90a01bbcad829d7
Daniele Coppola <daniele.coppola@inf.ethz.ch>
2025-02-27 11:13:48 +0100

    nerd font installation

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -9,3 +9,9 @@ sudo apt install waybar  grimshot
 sudo apt install tmux
 sudo apt install node npm
 npm install -g markdownlint-cli
+
+mkdir ~/.fonts
+
+cd ~/.fonts 
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/DejaVuSansMono.zip -O font.zip
+unzip font.zip
