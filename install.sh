5d935511fb11a23019ea14cb18951d1e190d438a
Daniele Coppola <daniele.coppola@inf.ethz.ch>
2025-03-11 07:37:49 +0000

    updated install.sh

--- install.sh
+++ install.sh
@@ -19,22 +19,26 @@ git clone https://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HO
 
 CWD=$(pwd)
 cd /tmp 
+
+echo "Download latest lazygit"
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit -D -t /usr/local/bin/
 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+echo "Download latest NVIM"
+curl -LO https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-linux-arm64.tar.gz
 sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
-
+sudo tar -C /opt -xvzf nvim-linux-arm64.tar.gz
+sudo chsh -s $(which zsh)
 
 cd $CWD
 
-exit
+git config --global user.email "daniele.coppola@inf.ethz.ch"
+git config --global user.name "Daniele Coppola"
 ## Begin STOW
 echo "Begin stowing"
-
+sudo $pakage_manager install stow
 for dir in $(find . -maxdepth 1 -mindepth 1 -type d); do
   pkg=$(basename $dir) 
   echo $pkg
