7f8bdba0ed367bf4805c794b626ec1d61c35ac93
Daniele Coppola <daniele.coppola@inf.ethz.ch>
2025-08-22 08:48:51 +0200

    nvim

--- nvim/.config/nvim/after/plugin/defaults.lua
+++ nvim/.config/nvim/after/plugin/defaults.lua
@@ -8,68 +8,6 @@ vim.keymap.set("n", "<leader>l", require("lazy").sync, { desc = "[L]azy" })
 vim.opt.foldnestmax = 3
 vim.opt.foldmethod = "indent"
 
-vim.api.nvim_create_autocmd("BufWritePost", {
-	pattern = "*.tex",
-	callback = function()
-		if vim.fn.executable("make") == 1 then
-			vim.fn.jobstart("make", {
-				detach = true,
-				on_stdout = function(_, data)
-					local output = {}
-					if data then
-						local i = 1
-						while i <= #data do
-							local line = data[i]
-							if
-								(line:match("^!") or line:match("[Ee]rror "))
-								and (not line:match("There was 1 error message"))
-							then
-								-- Add the matching line and the next two lines (if they exist) to the output
-								table.insert(output, line)
-								if i + 1 <= #data then
-									table.insert(output, data[i + 1])
-								end
-								if i + 2 <= #data then
-									table.insert(output, data[i + 2])
-								end
-								i = i + 2 -- Skip to after the extra lines to avoid redundant matching
-							end
-							i = i + 1
-						end
-					end
-					if #output > 0 then
-						vim.api.nvim_echo({ { table.concat(output, "\n"), "Normal" } }, true, {})
-					end
-				end,
-				on_stderr = function(_, data)
-					local output = {}
-					if data then
-						local i = 1
-						while i <= #data do
-							local line = data[i]
-							if line:match("^!") or line:match("[Ee]rror ") then
-								-- Add the matching line and the next two lines (if they exist) to the output
-								table.insert(output, line)
-								if i + 1 <= #data then
-									table.insert(output, data[i + 1])
-								end
-								if i + 2 <= #data then
-									table.insert(output, data[i + 2])
-								end
-								i = i + 2 -- Skip to after the extra lines to avoid redundant matching
-							end
-							i = i + 1
-						end
-					end
-					if #output > 0 then
-						vim.api.nvim_echo({ { table.concat(output, "\n"), "ErrorMsg" } }, true, {})
-					end
-				end,
-			})
-		end
-	end,
-})
-
 require("lspconfig").clangd.setup({
 	cmd = { "clangd" }, -- Specify the clangd binary
 	filetypes = { "c", "cpp", "objc", "objcpp" },
--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -17,4 +17,37 @@ return {
       require('diffview').setup()
     end,
   },
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+    },
+    config = function()
+      require('blink.cmp').setup()
+    end,
+  },
+
+  {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = 'zathura'
+      vim.g.vimtex_syntax_enabled = 0
+      vim.g.vimtex_compiler_latexmk = {
+        options = {
+          '-shell-escape',
+          '-verbose',
+          '-file-line-error',
+          '-synctex=1',
+          '-interaction=nonstopmode',
+        },
+      }
+    end,
+  },
 }
